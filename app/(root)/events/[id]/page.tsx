21255d73c89e638b9245915059af0fc7279164ad
Toluwani Owoade <toluwaniowoade@Tech-Toluwani.local>
2024-01-22 11:52:04 +0100

    Push

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -23,15 +23,17 @@ const EventDetails = async ({
 
   return (
     <>
-      <section className="flex justify-center bg-primary-50">
+      <section className="flex justify-center bg-primary-50 bg-dotted-pattern bg-contain">
         <div className="grid grid-cols-1 md:grid-cols-2 2xl:max-w-7xl">
-          <Image
-            src={event.imageUrl}
-            alt="hero image"
-            width={1000}
-            height={1000}
-            className="h-full"
-          />
+          <div className="max-w-full">
+            <Image
+              src={event.imageUrl}
+              alt="hero image"
+              width={1000}
+              height={1000}
+              className="h-full min-h-[300px] object-cover object-center"
+            />
+          </div>
 
           <div className="flex w-full flex-col gap-8 p-5 md:p-10">
             <div className="flex flex-col gap-6">
@@ -56,7 +58,7 @@ const EventDetails = async ({
               </div>
             </div>
 
-            <CheckoutButton event={event}  />
+            <CheckoutButton event={event} />
 
             <div className="flex flex-col gap-5">
               <div className="flex gap-2 md:gap-3">
